944e64e5dfa52fe81b7e5bdc41c6e6451777e10c
hxero <hxero@gmail.com>
2026-06-06 14:38:13 +0700

    init separate config

---
+++ config.lua
@@ -0,0 +1,14 @@
+return {
+	LAUNCH = "~/Project/hyprbadapple/box", -- launch command,
+
+	-- relative to current dir
+	BOX_PATH   = "baprocess/output/boxes.bin", -- path to the generated bin file
+	AUDIO_PATH = "baprocess/badapple.mp3",
+
+	MAX_BOXES = 153, -- the `most boxes` from python
+	FPS       = 30, -- 1-30 framerate (sync playback)
+
+	SCALE    = 24, -- scale  -> resolution height / (grid height)
+	OFFSET_X = 192, -- center -> [(resolution width) - (grid width * scale)] / 2
+	OFFSET_Y = 46, -- depends on your top|bottom bars - can be 0 if you want it to fullscreen
+};
